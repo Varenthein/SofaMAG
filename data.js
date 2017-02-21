3d7b7cf89ffcb2ef0982e74601e5422cdfa7420d
Paweł Zagrobelny <raventhein@gmail.com>
2017-02-21 14:20:37 +0100

    Added templating system

--- data.js
+++ data.js
@@ -1,8 +1,37 @@
-var data = [{ "id": "1", "title": "SofaMag nr 4", "pages": [
-    { "page": "1", "content": "<img src='source/--4/cover.png' style='width:100%'>"},
-    { "page": "2", "content": "Page 2"},
-    { "page": "3", "content": "Page 3"},
-    { "page": "4", "content": "Page 4"}
+//templates
+
+/* Sofa template */
+
+const title_page = function(number) {
+  this.img = "";
+  this.get = () => `<img src='source/--${number}/cover.png' style='width:100%' alt="">`;
+}
+
+const page = function(text = "Empty", type = "content", title = "Untitled", author = "Unknown", category = "No category", main_image = "") {
+  this.text = text;
+  this.type = type;
+  this.title = title;
+  this.author = author;
+  this.category = category;
+  this.main_image = main_image;
+  this.get = () => {
+  if(this.type != "with_image") return `<div class="text">${this.text}</div>`;
+  else { return `
+  <header style="background:url('${main_image}') cover">
+    ${title}
+  </header>
+  <img src='source/--${number}/cover.png' style='width:100%' alt="">`; }
+}
+}
+
+//data
+
+
+let data = [{ "id": "1", "title": "SofaMag nr 4", "pages": [
+    { "page": "0", "content": new title_page(4).get()},
+    { "page": "1", "content": new page("page 2").get()},
+    { "page": "2", "content": new page("page 3").get()},
+    { "page": "3", "content": new page("page 4").get()}
   ]
 }
 ]
--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-var resizeMag = function() {
+const container = document.querySelector('.container'); //get magazine container
+const outer = document.querySelector('.outer'); //get outer
 
-    var container = document.querySelector('.container'); //get magazine container
-    var outer = document.querySelector('.outer'); //get outer
+const resizeMag = function() {
 
-    var width = window.innerWidth; //get browser window width
-    var height = window.innerHeight; //get browser window height
-    var scale;
+    let width = window.innerWidth; //get browser window width
+    let height = window.innerHeight; //get browser window height
+    let scale;
 
     if(container.clientHeight > height) { //if container doesn't fit the browser window
       scale = Math.min(width/container.clientWidth, height/container.clientHeight); //get the scale
@@ -21,13 +21,46 @@ resizeMag(); //fire reziseing at the start
 
 //Generate bookblock
 
-var book = document.querySelector("#bb-bookblock");
+let book = document.querySelector("#bb-bookblock");
 
 window.$_GET = location.search.substr(1).split("&").reduce((o,i)=>(u=decodeURIComponent,[k,v]=i.split("="),o[u(k)]=v&&u(v),o),{}); //get data from $_GET
 
 if($_GET['id'] == "undefined") { //if id specified
   document.querySelector('body').innerHTML = "<p>There is no such file...</p>";
 } else {
-  var issue = data.filter((item) => item.id == $_GET['id']);
-  console.log(issue);
+  let issue = data.filter(item => item.id == $_GET['id'] );
+  if(issue.length < 1) document.querySelector('body').innerHTML = "<p>There is no such file...</p>"; //if there is no book with such id
+  else {
+
+    book.innerHTML = `
+      <div class="bb-item" style="background:transparent">
+        <div class="bb-custom-side" style="background:transparent">
+        </div>
+        <div class="bb-custom-side" style="">
+        ${issue[0].pages[0].content}
+        </div>
+      </div>
+      `;
+
+
+    book.innerHTML += issue[0].pages.slice(1).map((item, i) => {
+
+       let html = '';
+       if(i%2 == 0) html += `<div class="bb-item">`;
+       html += `<div class="bb-custom-side" style="background:transparent">${item.content}
+       </div>`;
+       if(i%2 != 0) html += `</div>`;
+       return `
+       ${(i%2==0) ? '<div class="bb-item">' : ''}
+          <div class="bb-custom-side" style="background:transparent">
+            ${item.content}
+          </div>
+       ${(i%2!=0) ? '</div>' : ''}
+       `;
+
+    }).join('');
+
+    console.log(test);
+
+  }
 }
