d7bdcb36e0314d4117ff2b942aef354b44b71246
Paweł Zagrobelny <raventhein@gmail.com>
2017-02-23 11:47:24 +0100

    Reorganized structure

--- data.js
+++ data.js
@@ -1,42 +1,5 @@
-//templates
-
-/* Sofa template */
-
-const title_page = function(number) {
-  this.img = "";
-  this.get = () => `<img src='source/--${number}/cover.png' style='width:100%' alt="">`;
-}
-
-const image = function(number, img) {
-  this.img = img;
-  this.get = () => `<img src='source/--${number}/${img}' style='width:100%' alt="">`;
-}
-
-const page = function(text = "Empty", type = "content", title = "Untitled", author = "Unknown", category = "No category", main_image = "") {
-  this.text = text;
-  this.type = type;
-  this.title = title;
-  this.author = author;
-  this.category = category;
-  this.main_image = main_image;
-  this.get = () => {
-  if(this.type != "with_image") return `<div class="text">${this.text}</div>`;
-  else { return `
-  <header style="background:url('${main_image}') cover">
-    ${title}
-  </header>
-  <img src='source/--${number}/cover.png' style='width:100%' alt="">`; }
-}
-}
-
 //data
 
-
-let data = [{ "id": "1", "title": "SofaMag nr 4", "pages": [
-    { "page": "0", "content": new title_page(4).get()},
-    { "page": "1", "content": "" },
-    { "page": "2", "content": new image(4, '2.png').get()},
-    { "page": "3", "content": new page("page 4").get()}
-  ]
-}
+let data = [
+  { "id": "1", "title": "SofaMag nr 4", "nr": "4", "url": "source/--4" }
 ]
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container'); //get magazine container
 const outer = document.querySelector('.outer'); //get outer
 
+//Resizeing mag dunction
 const resizeMag = function() {
 
     let width = window.innerWidth; //get browser window width
@@ -18,10 +19,62 @@ const resizeMag = function() {
 window.addEventListener('resize', resizeMag, true); //on window resize make magazine container fit the browser window
 resizeMag(); //fire reziseing at the start
 
+//Adding files function
+
+function loadFile(file_name, file_type){
+    if (file_type=="js"){ //if filename is a external JavaScript file
+        let file=document.createElement('script')
+        file.setAttribute("src", file_name)
+        file.setAttribute("type", "text/javascript")
+        file.onload = function () {
+          readPages(pages);
+        }
+        document.querySelector('head').appendChild(file);
+    }
+    else if (file_type=="css"){ //if filename is an external CSS file
+        var file=document.createElement("link")
+        file.setAttribute("rel", "stylesheet")
+        file.setAttribute("type", "text/css")
+        file.setAttribute("href", file_name)
+        document.head.appendChild(file);
+    }
+
+}
 
 //Generate bookblock
 
 let book = document.querySelector("#bb-bookblock");
+
+function readPages(pages) {
+
+  book.innerHTML = `
+    <div class="bb-item" style="background:transparent">
+      <div class="bb-custom-side" style="background:transparent">
+      </div>
+      <div class="bb-custom-side" style="">
+      ${pages[0].content}
+      </div>
+    </div>
+    `;
+
+  book.innerHTML += pages.slice(1).map((item, i) => {
+
+     let html = '';
+     if(i%2 == 0) html += `<div class="bb-item">`;
+     html += `<div class="bb-custom-side" style="background:transparent">${item.content}
+     </div>`;
+     if(i%2 != 0) html += `</div>`;
+     return `
+     ${(i%2==0) ? '<div class="bb-item">' : ''}
+        <div class="bb-custom-side">
+          ${item.content}
+        </div>
+     ${(i%2!=0) ? '</div>' : ''}
+     `;
+
+  }).join('');
+
+}
 
 window.$_GET = location.search.substr(1).split("&").reduce((o,i)=>(u=decodeURIComponent,[k,v]=i.split("="),o[u(k)]=v&&u(v),o),{}); //get data from $_GET
 
@@ -32,35 +85,9 @@ if($_GET['id'] == "undefined") { //if id specified
   if(issue.length < 1) document.querySelector('body').innerHTML = "<p>There is no such file...</p>"; //if there is no book with such id
   else {
 
-    book.innerHTML = `
-      <div class="bb-item" style="background:transparent">
-        <div class="bb-custom-side" style="background:transparent">
-        </div>
-        <div class="bb-custom-side" style="">
-        ${issue[0].pages[0].content}
-        </div>
-      </div>
-      `;
-
-
-    book.innerHTML += issue[0].pages.slice(1).map((item, i) => {
-
-       let html = '';
-       if(i%2 == 0) html += `<div class="bb-item">`;
-       html += `<div class="bb-custom-side" style="background:transparent">${item.content}
-       </div>`;
-       if(i%2 != 0) html += `</div>`;
-       return `
-       ${(i%2==0) ? '<div class="bb-item">' : ''}
-          <div class="bb-custom-side" style="background:transparent">
-            ${item.content}
-          </div>
-       ${(i%2!=0) ? '</div>' : ''}
-       `;
-
-    }).join('');
-
-    console.log(test);
+    //load styling files and data
+    loadFile(`${issue[0].url}/pages.js`,"js");
+    loadFile(`${issue[0].url}/style.css`,"css");
 
   }
 }
