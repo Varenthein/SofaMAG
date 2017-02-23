e02f038da81ffbc1d00af8a94d47da72883bd41b
Paweł Zagrobelny <raventhein@gmail.com>
2017-02-23 15:32:52 +0100

    Changed Sofa Template

--- templates/Sofa/template.js
+++ templates/Sofa/template.js
@@ -10,31 +10,44 @@ const image = function(img) {
   this.get = () => `<img src='${issue.url}/img/${img}' style='width:100%' alt="">`;
 }
 
-const page = function(page_nr = 0, text = "Empty", type = "content", title = "Untitled", author = "Unknown", category = "No category", main_image = "") {
+const page = function(page_nr = 0, excerpt = "", text = "Empty", type = "content", title = "Untitled", author = "Unknown", category = "No category", main_image = "", tags = [], license) {
   this.text = text;
+  this.excerpt = excerpt;
   this.type = type;
   this.title = title;
   this.author = author;
   this.category = category;
   this.main_image = main_image;
+  this.tags = tags;
+  this.license = license;
   this.get = () => {
   if(this.type != "with_image") {
      return `
-     <div class="text">${this.text}</div>
+     <div class="text">
+      <div class="content">
+      ${this.text}
+      </div>
+    </div>
     `;
   }
   else {
     let title_len = this.title.length;
     let title_size = 100-this.title.length+'px';
+    console.dir(this.tags);
     return `
     <span class="nr"><strong>${page_nr}</strong></span>
     <header style="background:url('${issue.url}/img/${this.main_image}');background-size:cover;background-position:center center;">
       <div class="fog"></div>
-      <div class="cat">${this.category}</div>
+      <div class="cat"><span class="romb"></span>${this.category}</div>
       <h1 style="font-size:${title_size}">${this.title}</h1>
+      <div class="tags"><span class="license">${this.license}</span>${this.tags.join(", ")}</div>
     </header>
       <div class="text">
-        ${this.text}
+        <img alt="${this.author}" src="${issue.url}/img/authors/${this.author}.png">
+        <p><strong>${this.excerpt}</strong></p>
+         <div class="content">
+         ${this.text}
+         </div>
       </div>
     `; }
 }
